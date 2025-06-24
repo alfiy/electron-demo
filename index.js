a8861d0de827d19de95a106c0ed1f3db91754313
alfiy <alfiy@163.com>
2025-06-24 15:13:16 +0800

    vite + ant-design/icons 完整的示例

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function App() {
       <h1>Electron SVG Icon Demo</h1>
       <DownOutlined style={{ fontSize: 24 }} />
       <PlusOutlined style={{ fontSize: 24 }} />
+      <img src="./assets/icon.svg" alt="SVG Icon" style={{ width: 60, height: 60 }} />
     </div>
   );
 }
